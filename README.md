4cc63effa16d5126726fbb884117e877dcbc6726
Sandeep Giri <sandeepgiri@gmail.com>
2024-05-12 11:48:45 -0400

    Added my readme

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# Create Project
+1. virtualenv myenv
+2. . myenv/bin/activate #source 3. myenv/bin/activate
+4. pip install ollama
+5. pip freeze > requirements.txt
+6. deactivate
+7. pip install -r requirements.txt
--- src/run_llma.py
+++ src/run_llma.py
@@ -1,9 +1,12 @@
 
 import ollama
-response = ollama.chat(model='llama3', messages=[
+response = ollama.chat(model='gemma:2b', messages=[
   {
     'role': 'user',
     'content': 'Why is the sky blue?',
   },
 ])
 print(response['message']['content'])
+
+
+#ollama run gemma:2b
